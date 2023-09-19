dc0749955dc1167f37cb9ccede70240d2dbf2221
sinooox <dima.i.fedulov@gmail.com>
2023-09-19 20:09:55 +0700

    1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#12
+import math
+
+#1
+def square():
+    n = int(input('N: '))
+    result = ''
+
+    for i in range(n+1):
+        if i == 0:
+            continue
+
+        if i**2 <= n:
+            result += f'{i**2} '
+
+    print(result)
+
+square()
